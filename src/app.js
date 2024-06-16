624333d71b4ec62eea6722605cc6c00ac4ecc1a6
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-16 13:19:06 +0530

    added the index file and the  middleware

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import router from "./routes/index.route.js";
+
+export const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use("/api/v1",router)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "../env",
+});
+connectDB()
+  .then(() => {
+    try {
+      app.on("error", () => {
+        throw error;
+      });
+      app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port: ${process.env.PORT}`);
+      });
+    } catch (err) {
+      console.error("something went wrong while connecting express " + err);
+    }
+  })
+  .catch((err) => {
+    console.log("database connection failed" + err);
+  });
