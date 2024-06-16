cff7a83c60476496f981907ef56f81bdb2e7d105
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-16 16:38:08 +0530

    added the flow algo with redis caching

--- src/utils/flowAlgorithm/flowAlgorithm.js
+++ src/utils/flowAlgorithm/flowAlgorithm.js
@@ -1,5 +1,5 @@
 
-class FlowDistributionAlgorithm {
+/*class FlowDistributionAlgorithm {
     constructor() {
         this.astrologers = [];
         this.totalConnections = 0;
@@ -36,3 +36,81 @@ class FlowDistributionAlgorithm {
 }
 
 export default new FlowDistributionAlgorithm();
+*/
+import redisClient from './redisClient';
+
+class FlowDistributionAlgorithm {
+    constructor() {
+        this.totalConnectionsKey = 'totalConnections';
+    }
+
+    async initialize() {
+        const totalConnections = await redisClient.get(this.totalConnectionsKey);
+        //check for the totalconnections
+        this.totalConnections = totalConnections ? parseInt(totalConnections, 10) : 0;
+    }
+    async addAstrologer(astrologer) {
+        const astrologerKey = `astrologer:${astrologer.id}`;
+
+        const existingAstrologer = await redisClient.hGetAll(astrologerKey);
+        if (existingAstrologer && Object.keys(existingAstrologer).length !== 0) {
+            throw new Error('Astrologer already exists');
+        }
+
+        await redisClient.hSet(astrologerKey, {
+            id: astrologer.id,
+            email: astrologer.email,
+            isTopAstrologer: astrologer.isTopAstrologer,
+            toggleAstrologer: astrologer.toggleAstrologer,
+            currentConnections: astrologer.currentConnections || 0,
+        });
+    }
+    async removeAstrologer(astrologerId) {
+        const astrologerKey = `astrologer:${astrologerId}`;
+        const astrologer = await redisClient.hGetAll(astrologerKey);
+        if (!astrologer || Object.keys(astrologer).length === 0) {
+            throw new Error('Astrologer not found');
+        }
+
+        await redisClient.del(astrologerKey);
+    }
+    async distributeUser(user) {
+        const astrologerKeys = await redisClient.keys('astrologer:*');
+        if (astrologerKeys.length === 0) {
+            throw new Error('No astrologers available');
+        }
+
+        let astrologers = await Promise.all(astrologerKeys.map(key => redisClient.hGetAll(key)));
+        astrologers = astrologers.map(astrologer => ({
+            ...astrologer,
+            currentConnections: parseInt(astrologer.currentConnections, 10),
+            isTopAstrologer: astrologer.isTopAstrologer === 'true',
+            toggleAstrologer: astrologer.toggleAstrologer === 'true'
+        }));
+
+        // Sort astrologers by the number of current connections
+        astrologers.sort((a, b) => a.currentConnections - b.currentConnections);
+
+        //filter both oof the astrologers aside 
+        const topAstrologers = astrologers.filter(a => a.isTopAstrologer && a.toggleAstrologer);
+        const regularAstrologers = astrologers.filter(a => !(a.isTopAstrologer && a.toggleAstrologer));
+        //distribute the user to the astrologer
+        const selectedAstrologer = topAstrologers.length && this.totalConnections % 2 === 0
+            ? topAstrologers[0]
+            : regularAstrologers[0];
+
+        selectedAstrologer.currentConnections++;
+        this.totalConnections++;
+
+        const selectedAstrologerKey = `astrologer:${selectedAstrologer.id}`;
+        await redisClient.hSet(selectedAstrologerKey, 'currentConnections', selectedAstrologer.currentConnections);
+        await redisClient.set(this.totalConnectionsKey, this.totalConnections);
+
+        return selectedAstrologer;
+    }
+}
+
+const flowDistributionAlgorithm = new FlowDistributionAlgorithm();
+flowDistributionAlgorithm.initialize(); // Initialize the algorithm
+
+export default flowDistributionAlgorithm;
