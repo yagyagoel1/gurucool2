3a6ce4945b8d21a52d666ee37023b2a4566199af
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-17 18:20:47 +0530

    added the rate limit and a health check route

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import router from "./routes/index.route.js";
-
+import {rateLimit} from "express-rate-limit";
 export const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  message: "Too many requests from this IP, please try again after 15 minutes",});
 
 app.use(
   cors({
@@ -16,6 +23,9 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use("/api/v1",router)
+app.get("/", (req, res) => {
+  res.status(200).send("is healthy");
+});
 
 
 
